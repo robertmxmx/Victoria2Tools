71ac3bdd472d802f04e38806fe60b6b4f16491ab
robertmxmx <70693574+robertmxmx@users.noreply.github.com>
2021-01-10 19:31:19 +1100

    Update Victoria2_Reader.py
    added ability to not include vassals in GDP or Population

--- Victoria2_Reader.py
+++ Victoria2_Reader.py
@@ -1,44 +1,45 @@
+
 import re
 import csv
 
 ############################################################################################################################################################
 # Created By Robert Milligan 2021 You may use and change this tool for any project you wish but please credit me in any produced works/progams/videos/posts#
 ############################################################################################################################################################
-#you must check and change the following things for the program to work#
+# you must check and change the following things for the program to work#
 ########################################################################
 # 1. find the countryfile for vanilla or the mod you are using
-        #it will be in Victoria 2/common/countries.txt
-        #or if using a mod which overides it use Victoria 2/*modname*/common/countries.txt
+# it will be in Victoria 2/common/countries.txt
+# or if using a mod which overides it use Victoria 2/*modname*/common/countries.txt
 countryfile_location = "C:/Program Files (x86)/Steam/steamapps/common/Victoria 2/mod/HPM/common/countries.txt"
 
 # 2. Find the savegame locations
-    #saves must be named 1.v2, 2.v2, 3.v2, 4.v2 etc..
+# saves must be named 1.v2, 2.v2, 3.v2, 4.v2 etc..
 save_game_folder = "C:/Users/granc/Documents/Paradox Interactive/Victoria II/HPM/save games"
 
 # 3. Length of Game Saves
-    #these 2 numbers must multiply together to make the total number of saves
+# these 2 numbers must multiply together to make the total number of saves
 saves_per_year = 2
 years_of_game = 100
 start_year = 1836
 
-#4. Optionals
-divide_all_results_by = 1 #set 1 for units, 1000 for thousands, 1000000 for millions etc.
-remove_0_tags = True #removes tags from output if all results 0 meaning tag was never on the map or created
-output_file_name = "output" #change this to whatever you want the output you want the csv file to be
-
-#5. Type of data to grab
-#you can only set one of the functions to True at once
+# 4. Type of data to grab
+# you can only set one of the functions to True at once
 Population_Reader_Bool = False
 GDP_Reader_Bool = True
 Culture_Religion_Reader_Bool = False
 
-#6. Optionals for Culture_Religion_Reader
-Use_All_Tags = False #set to False if you want to use "Use_Only_These_Tags" otherwise if it is True it will not care about the tag
-Use_Only_These_Tags = ["USA"] #can be 1 tag or a list of them e.g. ["ENG,"FRA","USA"]
+# 5. Optionals
+include_vassal_in_overlord = True # puts vassal data into overlord data for GDP and Population
+divide_all_results_by = 1  # set 1 for units, 1000 for thousands, 1000000 for millions etc.
+remove_0_tags = True  # removes tags from output if all results 0 meaning tag was never on the map or created
+output_file_name = "output"  # change this to whatever you want the output you want the csv file to be
+Use_All_Tags = False  # set to False if you want to use "Use_Only_These_Tags" otherwise if it is True it will not care about the tag
+Use_Only_These_Tags = ["USA"]  # can be 1 tag or a list of them e.g. ["ENG,"FRA","USA"]
 
 ##############################################
-year_list = ["blah","blah"]
-country_list = [["XXX","Indigenous People"]]
+year_list = ["blah", "blah"]
+country_list = [["XXX", "Indigenous People"]]
+
 
 def read_in_countries():
     for line in open(countryfile_location):
@@ -52,20 +53,21 @@ def read_in_countries():
         if firstsplit[0] == "dynamic_tags  ":
             continue
 
-        secondsplit = re.split("/|\"",firstsplit[1])
+        secondsplit = re.split("/|\"", firstsplit[1])
         english_name = secondsplit[2].split(".")[0]
-        country_list.append([tag,english_name])
+        country_list.append([tag, english_name])
 
     for i in range(years_of_game):
         for i in range(saves_per_year):
-            year_list.append(start_year+i)
+            year_list.append(start_year + i)
         for i in range(len(country_list)):
             for j in range(saves_per_year):
                 country_list[i].append(0)
 
+
 def Population_Reader():
-    for l in range(1, 1 + (saves_per_year*years_of_game)):
-        print(str(int(100*l/(saves_per_year*years_of_game)))+"%")
+    for l in range(1, 1 + (saves_per_year * years_of_game)):
+        print(str(int(100 * l / (saves_per_year * years_of_game))) + "%")
         filename = save_game_folder + "/" + str(l) + ".v2"
         province_end_tag = True
         savefile = open(filename, encoding='ISO-8859-1')
@@ -94,40 +96,40 @@ def Population_Reader():
                     pop_size = int(line[7:-1]) * 4 / divide_all_results_by
                     country_list[current_tag_index][l + 1] += pop_size
 
+        if include_vassal_in_overlord:
+            savefile = open(filename, encoding='ISO-8859-1')
+            last_vassal = -5
+            p = 0
+            for line in savefile:
 
+                p += 1
+                if line[1:8] == "vassal=" or line[1:10] == "substate=":
+                    last_vassal = p
 
-        savefile = open(filename, encoding='ISO-8859-1')
-        last_vassal = -5
-        p = 0
-        for line in savefile:
+                if p == last_vassal + 2:
+                    master_tag = line[9:12]
 
-            p += 1
-            if line[1:8] == "vassal=" or line[1:10] == "substate=":
-                last_vassal = p
+                if p == last_vassal + 3:
+                    vassal_tag = line[10:13]
+                    master_index = None
+                    vassal_index = None
+                    for m in range(len(country_list)):
+                        if country_list[m][0] == master_tag:
+                            master_index = m
+                        if country_list[m][0] == vassal_tag:
+                            vassal_index = m
 
-            if p == last_vassal + 2:
-                master_tag = line[9:12]
+                    if master_index == None:
+                        print(master_tag)
+                    if vassal_index == None:
+                        print(vassal_tag)
 
-            if p == last_vassal + 3:
-                vassal_tag = line[10:13]
-                master_index = None
-                vassal_index = None
-                for m in range(len(country_list)):
-                    if country_list[m][0] == master_tag:
-                        master_index = m
-                    if country_list[m][0] == vassal_tag:
-                        vassal_index = m
+                    country_list[master_index][l + 1] += country_list[vassal_index][l + 1]
+                    country_list[vassal_index][l + 1] = 0
 
-                if master_index == None:
-                    print(master_tag)
-                if vassal_index == None:
-                    print(vassal_tag)
+                    master_tag = None
+                    vassal_tag = None
 
-                country_list[master_index][l + 1] += country_list[vassal_index][l + 1]
-                country_list[vassal_index][l + 1] = 0
-
-                master_tag = None
-                vassal_tag = None
 
 def GDP_Reader():
     for l in range(1, 1 + (saves_per_year * years_of_game)):
@@ -182,40 +184,40 @@ def GDP_Reader():
                     country_list[current_tag_index][l + 1] -= float(line[17:]) / 1000 * 365.25 / divide_all_results_by
                 elif line[3:15] == "last_income=":
                     country_list[current_tag_index][l + 1] += float(line[15:]) / 1000 * 365.25 / divide_all_results_by
+        if include_vassal_in_overlord:
+            savefile = open(filename, encoding='ISO-8859-1')
+            last_vassal = -5
+            p = 0
+            for line in savefile:
 
+                p += 1
+                if line[1:8] == "vassal=" or line[1:10] == "substate=":
+                    last_vassal = p
 
-        savefile = open(filename, encoding='ISO-8859-1')
-        last_vassal = -5
-        p = 0
-        for line in savefile:
+                if p == last_vassal + 2:
+                    master_tag = line[9:12]
 
-            p += 1
-            if line[1:8] == "vassal=" or line[1:10] == "substate=":
-                last_vassal = p
+                if p == last_vassal + 3:
+                    vassal_tag = line[10:13]
+                    master_index = None
+                    vassal_index = None
+                    for m in range(len(country_list)):
+                        if country_list[m][0] == master_tag:
+                            master_index = m
+                        if country_list[m][0] == vassal_tag:
+                            vassal_index = m
 
-            if p == last_vassal + 2:
-                master_tag = line[9:12]
+                    if master_index == None:
+                        print(master_tag)
+                    if vassal_index == None:
+                        print(vassal_tag)
 
-            if p == last_vassal + 3:
-                vassal_tag = line[10:13]
-                master_index = None
-                vassal_index = None
-                for m in range(len(country_list)):
-                    if country_list[m][0] == master_tag:
-                        master_index = m
-                    if country_list[m][0] == vassal_tag:
-                        vassal_index = m
+                    country_list[master_index][l + 1] += country_list[vassal_index][l + 1]
+                    country_list[vassal_index][l + 1] = 0
 
-                if master_index == None:
-                    print(master_tag)
-                if vassal_index == None:
-                    print(vassal_tag)
+                    master_tag = None
+                    vassal_tag = None
 
-                country_list[master_index][l + 1] += country_list[vassal_index][l + 1]
-                country_list[vassal_index][l + 1] = 0
-
-                master_tag = None
-                vassal_tag = None
 
 def Culture_Religion():
     for l in range(1, 1 + (saves_per_year * years_of_game)):
@@ -262,6 +264,7 @@ def Culture_Religion():
                         pop_size = int(line[7:-1]) * 4 / divide_all_results_by
                         size_flag = True
 
+
 if __name__ == "__main__":
     read_in_countries()
 
@@ -286,3 +289,4 @@ if __name__ == "__main__":
         writer = csv.writer(f)
         writer.writerow(year_list)
         writer.writerows(country_list)
+
